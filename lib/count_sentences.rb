b867d9edbdc0101f839ef69cdbea42ffa2ed441e
bonveeee <bonochieng@gmail.com>
2022-08-20 15:14:11 +0300

    commit tree e4cadbcd5aa2831907cb701f9f30b01635998b0f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.include?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+self.split
   end
 
   def count_sentences
